53205343d1dd4423da3ca9cb797c67ef85d3578c
sercanyesilkoy@hufs.ac.kr <sercanyesilkoy@gmail.com>
2023-06-07 23:15:39 +0900

    added counters for person,bus,car and added text on boundry boxes

--- mainFile.py
+++ mainFile.py
@@ -49,4 +49,20 @@ while True:
             x1, y1, x2, y2 = int(x1),int(y1),int(x2),int(y2)
             w,h = x2-x1, y2-y1 #weight and height
             print(x1,y1,x2,y2)
+            
+            currentClass = classNames[cls]
+
+            conf = math.ceil((box.conf[0]*100))/100
+
+            if currentClass == "car" or currentClass == "person" or currentClass == "bus"\
+                    or currentClass == "truck" and conf > 0.1:
+                print(conf,currentClass)
+                cvzone.putTextRect(frame, f'{conf} {currentClass}', (max(0, x1), max(35, y1)),
+                                   scale=1.5, thickness=2)  # edited
+                if currentClass == "car":
+                    car_counter += 1
+                elif currentClass == "person":
+                    person_counter += 1
+                elif currentClass == "truck" or currentClass == "truck":
+                    truckOrBus_counter += 1            
 
