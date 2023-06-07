27a9a472553295eb16fd757c0c05d70014b9098c
sercanyesilkoy@hufs.ac.kr <sercanyesilkoy@gmail.com>
2023-06-07 19:52:04 +0900

    main file created

---
+++ mainFile.py
@@ -0,0 +1,32 @@
+from ultralytics import YOLO
+import cv2
+from cv2 import *
+
+import cvzone
+import numpy as np
+import math
+from sort import *
+from yolov5 import YOLOv5
+
+
+# cap = cv2.VideoCapture(0)
+cap = cv2.VideoCapture("../Videos/deneme3.MOV")  # For Video
+# cap.set(3,1280)
+# cap.set(4,720)
+model = YOLO('yolov8n.pt')
+
+classNames = ["person", "bicycle", "car", "motorbike", "aeroplane", "bus", "train", "truck", "boat",
+              "traffic light", "fire hydrant", "stop sign", "parking meter", "bench", "bird", "cat",
+              "dog", "horse", "sheep", "cow", "elephant", "bear", "zebra", "giraffe", "backpack", "umbrella",
+              "handbag", "tie", "suitcase", "frisbee", "skis", "snowboard", "sports ball", "kite", "baseball bat",
+              "baseball glove", "skateboard", "surfboard", "tennis racket", "bottle", "wine glass", "cup",
+              "fork", "knife", "spoon", "bowl", "banana", "apple", "sandwich", "orange", "broccoli",
+              "carrot", "hot dog", "pizza", "donut", "cake", "chair", "sofa", "pottedplant", "bed",
+              "diningtable", "toilet", "tvmonitor", "laptop", "mouse", "remote", "keyboard", "cell phone",
+              "microwave", "oven", "toaster", "sink", "refrigerator", "book", "clock", "vase", "scissors",
+              "teddy bear", "hair drier", "toothbrush"
+              ]
+
+tracker = Sort(max_age=20, min_hits=3,iou_threshold=0.3)
+limits = [535, 495, 1280, 470]
+countG = 0
